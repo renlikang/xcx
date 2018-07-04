6d2200dc9db59b0b97bcc40e4520d83b078cfc0e
任励康 <renlikang1986@163.com>
2018-07-04 17:02:34 +0800

    add

---
+++ app/common/config/main-prod.php
@@ -0,0 +1,35 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
+defined('YII_ENV_DEV') or define('YII_ENV_DEV', false);
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=dhsxs',
+            'username' => 'root',
+            'password' => '3659900',
+            'charset' => 'utf8',
+            'driverName' => 'mysql',
+        ],
+
+        'redis' => [
+            'class' => 'common\components\Redis',
+            'hostname' => '127.0.0.1',
+            'password' => '',
+            'port' => '6379',
+            'database' => 1,
+            'socketClientFlags' => STREAM_CLIENT_CONNECT
+        ],
+
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
--- app/frontend/config/main-local.php
+++ app/frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'eNzNlpuSz_ezpT7QUs6fd2PFSNaxrB5f',
         ],
     ],
 ];
