b0c1d70ffb218f59b338bf8f13ab27f1e3168924
任励康 <renlikang1986@163.com>
2018-07-12 13:44:24 +0800

    add

--- app/common/config/params.php
+++ app/common/config/params.php
@@ -3,5 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'staticUrl' => 'http://file.xian10.com'
+    'staticUrl' => 'https://file.xian10.com'
 ];
