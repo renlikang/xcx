fc1d5380a72fca0fb76c1f31487fcdc96a352b56
renlikang <renlikang1986@163.com>
2018-11-16 11:45:33 +0800

    add

--- app/api/actions/my/Img.php
+++ app/api/actions/my/Img.php
@@ -10,22 +10,6 @@ use Yii;
 
 class Img extends BaseAction
 {
-    /**
-     * @SWG\Post(
-     *     path="/my/img",
-     *     tags={"我的照片功能"},
-     *     summary="冲洗照片接口",
-     *     description="",
-     *     produces={"application/json"},
-     *     @SWG\Parameter(in = "formData",name = "address",description = "地址",required = true,type = "string"),
-     *     @SWG\Parameter(in = "formData",name = "memo",description = "描述",required = true,type = "string"),
-     *     @SWG\Parameter(in = "formData",name = "imgUrl",description = "图片地址",required = true,type = "string"),
-     *     @SWG\Parameter(in = "formData",name = "title",description = "标题（预留字段, 可不传）",required = false,type = "string"),
-     *     @SWG\Response(response = 200,description = " success"),
-     *     @SWG\Response(response = 500,description = " 请登录"),
-     * )
-     *
-     */
     public function run()
     {
         if(Yii::$app->user->isGuest) {
