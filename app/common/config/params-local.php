8d6f1b30507ad046e0cb8b7a2400c49718b61a9c
renlikang <renlikang1986@163.com>
2019-01-02 15:34:09 +0800

    add

--- app/common/config/params-local.php
+++ app/common/config/params-local.php
@@ -1,13 +1,13 @@
 <?php
 if (YII_ENV == 'dev') {
     return [
-        'appid' => 'wx78717a7a86f51ae9',
-        'appsecret' => '63c6828e7d37a3b5288414c4cf92d5e2'
+        'appid' => 'wx05b3618541f14ba1',
+        'appsecret' => 'cfee799bb956a6f68290146e0207324c'
     ];
 } elseif (YII_ENV == 'test') {
     return [
-        'appid' => 'wx78717a7a86f51ae9',
-        'appsecret' => '63c6828e7d37a3b5288414c4cf92d5e2'
+        'appid' => 'wx05b3618541f14ba1',
+        'appsecret' => 'cfee799bb956a6f68290146e0207324c'
     ];
 } elseif (YII_ENV == 'prod') {
     return [
