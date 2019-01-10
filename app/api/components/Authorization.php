9b995a39e86d541b2ae6680ba416844fbe4ba71b
renlikang <renlikang1986@163.com>
2019-01-10 22:37:38 +0800

    add

--- app/api/components/Authorization.php
+++ app/api/components/Authorization.php
@@ -8,10 +8,9 @@ use yii\web\ForbiddenHttpException;
 
 class Authorization extends Component
 {
-    public function init() {
-        var_dump(2);
+    public function init()
+    {
         if ($token = Yii::$app->request->headers['authorization']) {
-            var_dump(1);exit;
             Yii::$app->user->loginByAccessToken($token);
         }
         exit;
