51268b176b1489d27a4daf6e4dab2c8e585bab08
renlikang <renlikang1986@163.com>
2019-03-25 15:12:33 +0800

    add

---
+++ app/api/controllers/GameController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renlikang
+ * Date: 2019/3/25
+ * Time: 3:07 PM
+ */
+
+namespace api\controllers;
+
+
+use yii\rest\Controller;
+
+class GameController extends Controller
+{
+    public function actionA()
+    {
+        return 1111;
+    }
+}
