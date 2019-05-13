c98febecb2e5850cd95100c28ef04a976b1498df
任励康 <renlikang1986@163.com>
2019-05-13 19:00:00 +0800

    add

--- app/api/config/params.php
+++ app/api/config/params.php
@@ -14,7 +14,7 @@ return [
             'index' => ['*'],
             'comment' => ['index'],
             'activity' => ['*'],
-            'article' =>['list']
+            'article' =>['*']
         ],
     ],
 ];
