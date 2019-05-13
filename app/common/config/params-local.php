664aa3b34738bfdd7d44ddae7b5af07b6040d308
任励康 <renlikang1986@163.com>
2019-05-13 18:52:05 +0800

    add

--- app/common/config/params-local.php
+++ app/common/config/params-local.php
@@ -11,7 +11,7 @@ if (YII_ENV == 'dev') {
     ];
 } elseif (YII_ENV == 'prod') {
     return [
-        'appid' => 'wx05b3618541f14ba1',
-        'appsecret' => 'cfee799bb956a6f68290146e0207324c'
+        'appid' => 'wx2f06e42b7389715b',
+        'appsecret' => '73dca7f64e79785c3081ae4b4d520241'
     ];
 }
